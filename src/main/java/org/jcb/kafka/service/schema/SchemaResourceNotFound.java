d6008e443b083b8ba53f957eb6746bf66ced9590
juanca <juanca737@gmail.com>
2022-03-28 18:54:51 -0300

    kafka producer and consumer clients demo

---
+++ src/main/java/org/jcb/kafka/service/schema/SchemaResourceNotFound.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022.
+ * Juan Barraza
+ */
+
+package org.jcb.kafka.service.schema;
+
+public class SchemaResourceNotFound extends Throwable {
+
+    public SchemaResourceNotFound(String message) {
+        super(message);
+    }
+
+}
